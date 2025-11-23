fde479ad92c7ee4d54db103337cdaad6c0b3e442
shadab057 <mohdshadab4629@gmail.com>
2025-11-24 01:02:52 +0530

    Add square root calculation example using math and cmath modules

---
+++ Day4/square.py
@@ -0,0 +1,8 @@
+# How can we calculate square root of -ve and +ve numbers in Python?
+# There are two modules in Python to deal with square roots: math and cmath
+import math as C
+print(C.sqrt(25))
+
+# cmath module deal with complex numbers
+import cmath as D
+print(D.sqrt(-25))
