86041e6c050057218518aae70b86643b0cff5851
shadab057 <mohdshadab4629@gmail.com>
2025-11-26 19:27:17 +0530

    Add content on error types and exception handling in Python, including examples for syntax and logical errors

---
+++ Day8/example.py
@@ -0,0 +1,6 @@
+a=10 
+b=15
+c= a+
+  # ^ SyntaxError: invalid syntax   
+
+  
---
+++ Day8/example1.py
@@ -0,0 +1,10 @@
+a=10
+b=3
+c=a+b/2
+print(c)       #  Technically correct but logically wrong answer 11.5
+  
+
+a=10 
+b=15    
+c= (a+b)/2
+print(c)       # Correct answer 12.5
